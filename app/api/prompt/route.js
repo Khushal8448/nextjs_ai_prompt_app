9e1d36ca00d52d9ae5ec5eaeab20fb45ac98d0ad
Khushal Mali <khushalmali8448@gmail.com>
2024-03-25 12:16:03 +0530

    Added copy prompt fuctionality

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -5,7 +5,7 @@ export const GET = async () => {
   try {
     await connectToDB();
 
-    const prompts = await Prompt.find();
+    const prompts = await Prompt.find({}).populate("creator");
 
     return new Response(JSON.stringify(prompts), { status: 201 });
   } catch (error) {
